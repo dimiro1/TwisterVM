effd93d9a107100309761639f76da617114ba4fc
Claudemiro Alves <dimiro1@gmail.com>
2009-06-23 11:54:45 -0300

    menu options fixed

--- zen.cpp
+++ zen.cpp
@@ -5,21 +5,20 @@
 
 static char *execname; // executable name
 
-inline void 
-showCopyright () 
-{ 
-  cout << "zen - Copyright (C) 2009 Claudemiro Alves Feitosa Neto" << endl; 
+inline void
+showCopyright ()
+{
+  cout << "zen - Copyright (C) 2009 Claudemiro Alves Feitosa Neto" << endl;
 }
 
-void 
+void
 showUsage ()
 {
 
   fprintf(stderr,
   "usage: %s [options] [script file].\n"
   "Available options are:\n"
-  "  -e [execute] filename\t\texecute filename\n"
-  "  -d [disassemble] filename\tdisassemble code\n"
+  "  -d [disassemble] \t\tdisassemble code\n"
   "  -h [help]\t\t\tshow this help\n"
   "  -c [copyright]\t\tcopyright information\n\n"
   ,
@@ -29,46 +28,68 @@ showUsage ()
 
 }
 
-int 
+/* execute, disassemble, help and copyright flags */
+int eflag, dflag, hflag, cflag;
+
+int
 main (int argc, char **argv)
 {
   VM vm; // instantiate a new vm
+  int option_index = 0;
+  int c;
 
   execname = argv[0];
 
   static struct option long_options[] = {
-    {"copyright",   no_argument, 0, 'c'      },
-    {"disassemble", required_argument, 0, 'd'},
-    {"execute",     required_argument, 0, 'e'},
-    {"help",        no_argument, 0, 'h'      },
-    {0,0,0,0}
+	 {"copyright",   no_argument, 0, 'c'      },
+	 {"disassemble", no_argument, 0, 'd'},
+	 {"help",        no_argument, 0, 'h'      },
+	 {0,0,0,0}
   };
 
-  int option_index = 0;
-  int c;
-
-  while( (c = getopt_long( argc, argv, "cd:e:h", long_options, &option_index )) != -1) {
-    switch( c ) {
-    case 'c':
-      showCopyright ();
-      break;
-    case 'd':
-      vm.load (optarg);
-      vm.showBytecodes ();
-      break;
-    case 'e':
-      vm.load (optarg);
-      vm.run ();
-      break;
-    case 'h':
-    case '?':
-      showUsage ();
-      break;
-    default:
-      abort ();
-      break;
-    }
+  eflag = 1;
+  while( (c = getopt_long( argc, argv, "cdh", long_options, &option_index )) != -1) {
+	 switch( c ) {
+	 case 'c':
+		cflag = 1;
+		break;
+	 case 'd':
+		eflag = 0;
+		dflag = 1;
+		break;
+	 case 'h':
+	 case '?':
+		hflag = 1;
+		break;
+	 default:
+		hflag = 1;
+		break;
+	 }
   }
+
+  argv += optind;
+  if (argc < 2)
+	 {
+		showUsage ();
+		exit (0);
+	 }
+  if (cflag)
+	 {
+		showCopyright ();
+		exit (0);
+	 }
+  if (hflag)
+	 {
+		showUsage ();
+		exit (0);
+	 }
+  else
+	 {
+		vm.load (argv[0]);
+		if (dflag)
+		  vm.showBytecodes ();
+		if (eflag)
+		  vm.run ();
+	 }
   return 0;
 }
-
