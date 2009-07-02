5391687cea5b7abd0d7b4f47225d434885d67b82
Claudemiro Alves <dimiro1@gmail.com>
2009-07-01 23:07:23 -0300

    formatação

--- opcode.h
+++ opcode.h
@@ -2,36 +2,36 @@
 #define OPCODE_H
 #include <cstdlib>
 
-enum Opcode {
-    PUSH,
-    ADD,
-    SUB,
-    MULT,
-    DIV,
-    POP,
-    PRINT,
-    PUTS,
-    NOP, // no operation
-    RESET,
-    GETOP, // for puts and print
-    HALT
+enum Opcode
+{
+  PUSH,
+  ADD,
+  SUB,
+  MULT,
+  DIV,
+  POP,
+  PRINT,
+  PUTS,
+  NOP,				// no operation
+  RESET,
+  GETOP,			// for puts and print
+  HALT
 };
 
 /* used in showBytecodes */
 static const char *opcodeName[] = {
-    "PUSH",
-    "ADD",
-    "SUB",
-    "MULT",
-    "DIV",
-    "POP",
-    "PRINT",
-    "PUTS",
-    "NOP", // no operation
-    "RESET",
-    "GETOP",
-    "HALT"
+  "PUSH",
+  "ADD",
+  "SUB",
+  "MULT",
+  "DIV",
+  "POP",
+  "PRINT",
+  "PUTS",
+  "NOP",			// no operation
+  "RESET",
+  "GETOP",
+  "HALT"
 };
 
 #endif
-
